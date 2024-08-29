42cdfed1805359da1e5ad7df8dba2d4e80b819bd
MJ Zorick <maryjane.zorick@gmail.com>
2024-08-29 15:30:13 -0700

    renamed

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
     return (
         <>
             <Head>
-                <title>TS Discography</title>
+                <title>The Pixies!</title>
             </Head>
             <main>{children}</main>
         </>
